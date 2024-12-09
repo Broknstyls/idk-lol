f381f66b37a80a8ae89475dfaae0992056ad7bb9
Broknstyls <muban90@gmail.com>
2024-12-09 20:49:42 +0200

    created universal scoped thingies

--- fizz.cpp
+++ fizz.cpp
@@ -1,3 +1,6 @@
 #include<iostream>
 #include <cmath>
 #include <vector>
+int AllCardsUsed;
+std::vector <int> AvailableCards = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30};
+std::vector <int> UsedCards;
