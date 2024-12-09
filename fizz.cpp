7062dd199078e77f991ea07a487e2bd8cb61f34a
Broknstyls <muban90@gmail.com>
2024-12-09 21:04:26 +0200

    created to player randomizer function

--- fizz.cpp
+++ fizz.cpp
@@ -4,6 +4,17 @@
 int AllCardsUsed;
 std::vector <int> AvailableCards = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30};
 std::vector <int> UsedCards;
+int YourHand(std::vector <int> deck, std::vector <int> Used){
+    srand(time(NULL));
+    for (int i = 0; i <= 2; i++)
+    {
+        int randomizer = rand() % AvailableCards.size();
+        deck.at(i) =  AvailableCards.at(randomizer);
+        std::cout<< deck.at(i) << '\n';
+        AvailableCards.erase(AvailableCards.begin() + randomizer);
+    }
+    
+}
 int main(){
     std::cout<< "welcome to fizzbuzz roulette! "<< '\n';
     std::cout<< "your cards are: "<< '\n';
